a1edf7b43c33808a2e3e888be462426b59a570b4
bellabula <zz225864@gmail.com>
2024-12-08 17:54:38 +0800

    fixed member js after login

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,16 +2,16 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
-    $("#editProfile").click(() => {
-        $("#profile").addClass("d-none")
-        $("#profileEdit").removeClass("d-none")
-    })
-    $("#confirmEdit").click(() => {
-        $("#profileEdit").addClass("d-none")
-        $("#profile").removeClass("d-none")
-    })
 
     $(document).ready(function () {
+        $("#editProfile").click(() => {
+            $("#profile").addClass("d-none")
+            $("#profileEdit").removeClass("d-none")
+        })
+        $("#confirmEdit").click(() => {
+            $("#profileEdit").addClass("d-none")
+            $("#profile").removeClass("d-none")
+        })
         // 增加按鈕點擊事件
         $('#member .btn-increase').on('click', function () {
             let input = $(this).siblings('input');
@@ -35,70 +35,71 @@ export default function Dashboard() {
             $(this).toggleClass('active');
             $(this).find('input[type="checkbox"]').prop('checked', $(this).hasClass('active'));
         });
-    });
 
-    $(document).on('click', function (event) {
-        const classFilterList = [$('#memberStore .classFilter'), $('#memberWallet .classFilter'), $($('#memberWallet .itemFilter')[0]), $($('#memberWallet .itemFilter')[1])]
-        // 檢查點擊是否發生在目標元素外部
-        classFilterList.forEach(element => {
-            if (!element.is(event.target) && element.has(event.target).length === 0) {
-                element.css('visibility', 'hidden');
-            }
+        $(document).on('click', function (event) {
+            const classFilterList = [$('#memberStore .classFilter'), $('#memberWallet .classFilter'), $($('#memberWallet .itemFilter')[0]), $($('#memberWallet .itemFilter')[1])]
+            // 檢查點擊是否發生在目標元素外部
+            classFilterList.forEach(element => {
+                if (!element.is(event.target) && element.has(event.target).length === 0) {
+                    element.css('visibility', 'hidden');
+                }
+            })
+        });
+    
+        const filerBtnList = [[$("#memberStore .classFilterBtn"), $("#memberStore .classFilter")],
+        [$("#memberWallet .classFilterBtn"), $("#memberWallet .classFilter")],
+        [$($('#memberWallet .itemFilterBtn')[0]), $($('#memberWallet .itemFilter')[0])],
+        [$($('#memberWallet .itemFilterBtn')[1]), $($('#memberWallet .itemFilter')[1])]]
+        filerBtnList.forEach(element => {
+            element[0].click(() => {
+                event.stopPropagation();
+                element[1].css("visibility", "visible")
+            })
+        });
+    
+    
+        const dateInput = document.getElementById('dateInput');
+    
+        // 取得今天的日期，並格式化為 YYYY-MM-DD
+        const today = new Date();
+        const formattedDate = today.toISOString().split('T')[0];
+    
+        // #memberWallet 
+        $("#memberWallet .orderStartDate").prop("max", formattedDate)
+        $("#memberWallet .orderEndDate").prop("max", formattedDate)
+    
+        $("#memberWallet .orderStartDate").on("input", () => {
+            $("#memberWallet .orderStartText").val($("#memberWallet .orderStartDate").val())
+            $("#memberWallet .orderEndDate").prop("min", $("#memberWallet .orderStartDate").val())
+        })
+        $("#memberWallet .orderEndDate").on("input", () => {
+            $("#memberWallet .orderEndText").val($("#memberWallet .orderEndDate").val())
+            $("#memberWallet .orderStartDate").prop("max", $("#memberWallet .orderEndDate").val())
+        })
+    
+        // #memberOrder
+        $("#memberOrder .orderStartDate").prop("max", formattedDate)
+        $("#memberOrder .orderEndDate").prop("max", formattedDate)
+    
+        $("#memberOrder .orderStartDate").on("input", () => {
+            $("#memberOrder .orderStartText").val($("#memberOrder .orderStartDate").val())
+            $("#memberOrder .orderEndDate").prop("min", $("#memberOrder .orderStartDate").val())
+        })
+        $("#memberOrder .orderEndDate").on("input", () => {
+            $("#memberOrder .orderEndText").val($("#memberOrder .orderEndDate").val())
+            $("#memberOrder .orderStartDate").prop("max", $("#memberOrder .orderEndDate").val())
+        })
+    
+        $("#editPwdBtn").click(() => {
+            $("#pwdSection").css("display", "block")
+            $("#editPwdBtn").css("display", "none")
+        })
+        $("#confirmPwdBtn").click(() => {
+            $("#pwdSection").css("display", "none")
+            $("#editPwdBtn").css("display", "inline")
         })
     });
 
-    const filerBtnList = [[$("#memberStore .classFilterBtn"), $("#memberStore .classFilter")],
-    [$("#memberWallet .classFilterBtn"), $("#memberWallet .classFilter")],
-    [$($('#memberWallet .itemFilterBtn')[0]), $($('#memberWallet .itemFilter')[0])],
-    [$($('#memberWallet .itemFilterBtn')[1]), $($('#memberWallet .itemFilter')[1])]]
-    filerBtnList.forEach(element => {
-        element[0].click(() => {
-            event.stopPropagation();
-            element[1].css("visibility", "visible")
-        })
-    });
-
-
-    const dateInput = document.getElementById('dateInput');
-
-    // 取得今天的日期，並格式化為 YYYY-MM-DD
-    const today = new Date();
-    const formattedDate = today.toISOString().split('T')[0];
-
-    // #memberWallet 
-    $("#memberWallet .orderStartDate").prop("max", formattedDate)
-    $("#memberWallet .orderEndDate").prop("max", formattedDate)
-
-    $("#memberWallet .orderStartDate").on("input", () => {
-        $("#memberWallet .orderStartText").val($("#memberWallet .orderStartDate").val())
-        $("#memberWallet .orderEndDate").prop("min", $("#memberWallet .orderStartDate").val())
-    })
-    $("#memberWallet .orderEndDate").on("input", () => {
-        $("#memberWallet .orderEndText").val($("#memberWallet .orderEndDate").val())
-        $("#memberWallet .orderStartDate").prop("max", $("#memberWallet .orderEndDate").val())
-    })
-
-    // #memberOrder
-    $("#memberOrder .orderStartDate").prop("max", formattedDate)
-    $("#memberOrder .orderEndDate").prop("max", formattedDate)
-
-    $("#memberOrder .orderStartDate").on("input", () => {
-        $("#memberOrder .orderStartText").val($("#memberOrder .orderStartDate").val())
-        $("#memberOrder .orderEndDate").prop("min", $("#memberOrder .orderStartDate").val())
-    })
-    $("#memberOrder .orderEndDate").on("input", () => {
-        $("#memberOrder .orderEndText").val($("#memberOrder .orderEndDate").val())
-        $("#memberOrder .orderStartDate").prop("max", $("#memberOrder .orderEndDate").val())
-    })
-
-    $("#editPwdBtn").click(() => {
-        $("#pwdSection").css("display", "block")
-        $("#editPwdBtn").css("display", "none")
-    })
-    $("#confirmPwdBtn").click(() => {
-        $("#pwdSection").css("display", "none")
-        $("#editPwdBtn").css("display", "inline")
-    })
     return (
         <AuthenticatedLayout>
             <Head title="Member" />
